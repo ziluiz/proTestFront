1944265f3ac03d418e907e6fc14d7bfd197972d2
ziluiz <luiz021205@gmail.com>
2023-06-13 10:33:40 -0300

    sjadij

---
+++ src/app/anuncio/anuncio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anuncio',
+  templateUrl: './anuncio.component.html',
+  styleUrls: ['./anuncio.component.css']
+})
+export class AnuncioComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AnuncioComponent } from './anuncio/anuncio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnuncioComponent
   ],
   imports: [
     BrowserModule,
